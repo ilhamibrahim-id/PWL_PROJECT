991d583df7b794ef86786e52918def336d8396ac
ilhamibrahim-id <ii.ilhamibrahim@gmail.com>
2021-05-09 14:17:27 +0700

    Membuat desain database dengan migrate

---
+++ database/migrations/2021_05_09_070642_create_table_matakuliah.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableMatakuliah extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('table_matakuliah', function (Blueprint $table) {
+            $table->string('kode_mk');
+            $table->string('nama_mk');
+            $table->string('sks');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_matakuliah');
+    }
+}
---
+++ database/migrations/2021_05_09_070705_create_table_kelas.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableKelas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('table_kelas', function (Blueprint $table) {
+            $table->string("id_kelas");
+            $table->string("nama_kelas");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_kelas');
+    }
+}
