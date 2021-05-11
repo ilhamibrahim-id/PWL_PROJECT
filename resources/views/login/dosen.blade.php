f2b4445b02dd97da76ae6e3fd3763a0006282263
ilhamibrahim-id <ii.ilhamibrahim@gmail.com>
2021-05-11 12:51:50 +0700

    Menambahkan Footer biar makin keren gan

--- resources/views/login/dosen.blade.php
+++ resources/views/login/dosen.blade.php
@@ -30,6 +30,11 @@
                               </a>
                         </div>
                         </div>
+                        <footer class="bg-light text-center text-lg-start">
+                            <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+                              © 2021 Copyright: Clan Anckerman
+                            </div>
+                          </footer>
 				    </div>
 			    </div>
 		    </div>
